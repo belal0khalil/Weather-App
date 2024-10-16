f3f12a430016e4d91960aa2478a5ff7bc9fa6d6a
belal0Khalil <belalessam1236@gmail.com>
2024-10-16 14:34:50 +0200

    no commit

--- js/main.js
+++ js/main.js
@@ -1,49 +1,46 @@
 var searchInput = document.querySelector(".search-input");
-var weatherData;
- /// functions
+var tempcontainer = document.querySelector(".temp-container")
+var finaldata;
+         /// functions
  async function getData(key) {
     var response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=8ce6c084ae874b1dba8102406241410&q=${key}&days=3`)
-     var finaldata = await response.json()
-   return finaldata
-}
- async function startData(key) {
-    weatherData = await getData(key);
-    console.log(weatherData)
-    todayData();
+     finaldata = await response.json()
+     todayData();
     tommorrowdata();
     aftertommorrow();
-} 
+}
 function todayData() {
-    let date = new Date(weatherData.location.localtime);
+    let date = new Date(finaldata.location.localtime);
     document.querySelector(".day").innerHTML = date.toLocaleDateString("en-us" , {weekday: 'long'});
     document.querySelector(".dayofmounth").innerHTML = date.toLocaleDateString("en-us" , {day: '2-digit'});
     document.querySelector(".mounth").innerHTML = date.toLocaleDateString("en-us" , {month: 'long'});
-    document.querySelector(".city").innerHTML = weatherData.location.country;
-    document.querySelector(".temp").innerHTML = weatherData.current.temp_c+'°C';
-    document.getElementById("temp-image").setAttribute('src' ,'https:'+ weatherData.current.condition.icon );
-    document.querySelector(" .forecoast-status .main").innerHTML = weatherData.current.condition.text;
-    document.querySelector(".humidity").innerHTML= weatherData.current.wind_mph+'%';
-    document.querySelector(".winds").innerHTML= weatherData.current.wind_kph+'kh/h';
-    document.querySelector(".weatherTrend").innerHTML= weatherData.current.wind_dir;
+    document.querySelector(".city").innerHTML = finaldata.location.country;
+    document.querySelector(".temp").innerHTML = finaldata.current.temp_c+'°C';
+    document.getElementById("temp-image").setAttribute('src' ,'https:'+ finaldata.current.condition.icon );
+    document.querySelector(" .forecoast-status .main").innerHTML = finaldata.current.condition.text;
+    document.querySelector(".humidity").innerHTML= finaldata.current.wind_mph+'%';
+    document.querySelector(".winds").innerHTML= finaldata.current.wind_kph+'kh/h';
+    document.querySelector(".weatherTrend").innerHTML= finaldata.current.wind_dir;
 }
 function tommorrowdata() {
-    let date = new Date(weatherData.forecast.forecastday[1].date);
+    let date = new Date(finaldata.forecast.forecastday[1].date);
     document.querySelector(".tommorrowday").innerHTML = date.toLocaleDateString("en-us" , {weekday: 'long'});
-    document.querySelector(".degree .tommorrow-img").setAttribute('src' , 'https:'+weatherData.forecast.forecastday[1].day.condition.icon )
-    document.querySelector(".max").innerHTML = weatherData.forecast.forecastday[1].day.maxtemp_c+ "°C";
-    document.querySelector(".min-after").innerHTML = weatherData.forecast.forecastday[1].day.mintemp_c+ "°C";
-    document.querySelector(".weather-condition-tommorrow").innerHTML = weatherData.forecast.forecastday[1].day.condition.text;
+    document.querySelector(".degree .tommorrow-img").setAttribute('src' , 'https:'+finaldata.forecast.forecastday[1].day.condition.icon )
+    document.querySelector(".max").innerHTML = finaldata.forecast.forecastday[1].day.maxtemp_c+ "°C";
+    document.querySelector(".min-after").innerHTML = finaldata.forecast.forecastday[1].day.mintemp_c+ "°C";
+    document.querySelector(".weather-condition-tommorrow").innerHTML = finaldata.forecast.forecastday[1].day.condition.text;
 }
 function aftertommorrow() {
-    let date = new Date(weatherData.forecast.forecastday[2].date);
+    let date = new Date(finaldata.forecast.forecastday[2].date);
     document.querySelector(".aftertommorrowday").innerHTML = date.toLocaleDateString("en-us" , {weekday: 'long'});
-document.querySelector(".aftertommorrow-img").setAttribute("src" , 'https:'+weatherData.forecast.forecastday[2].day.condition.icon )
-document.querySelector(".max-after").innerHTML = weatherData.forecast.forecastday[2].day.maxtemp_c+ "°C";
-document.querySelector(".min-aftertommorrow ").innerHTML = weatherData.forecast.forecastday[2].day.mintemp_c+ "°C";
-document.querySelector(".weather-condition-aftertommorrow").innerHTML = weatherData.forecast.forecastday[1].day.condition.text;
+document.querySelector(".aftertommorrow-img").setAttribute("src" , 'https:'+finaldata.forecast.forecastday[2].day.condition.icon )
+document.querySelector(".max-after").innerHTML = finaldata.forecast.forecastday[2].day.maxtemp_c+ "°C";
+document.querySelector(".min-aftertommorrow ").innerHTML = finaldata.forecast.forecastday[2].day.mintemp_c+ "°C";
+document.querySelector(".weather-condition-aftertommorrow").innerHTML = finaldata.forecast.forecastday[1].day.condition.text;
 }
-//// Events
+           //// Events
 searchInput.addEventListener("keyup", function() {
-    startData(searchInput.value);
-    
-})
+   if(searchInput.value.length > 3) {
+    getData(searchInput.value);
+   }
+});
